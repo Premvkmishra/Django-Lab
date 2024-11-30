401edba945cc5acdb064be6c8621f27ae3e635da
Premvkmishra <premvkmishra@gmail.com>
2024-11-30 16:32:37 +0530

    models and migrations

--- base/models.py
+++ base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264, unique=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Messages(models.Model):
+    name = models.CharField(max_length=255)
+    messages = models.TextField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
